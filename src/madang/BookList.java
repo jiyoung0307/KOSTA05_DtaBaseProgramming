762cc6a4a3a584600448c7463b76abcf6c6cf841
zy_zero <jiyoungjang0307@gmail.com>
2023-09-11 10:10:06 +0900

    Database프로그래밍 v.1

---
+++ src/madang/BookList.java
@@ -0,0 +1,4 @@
+package madang;
+
+public class BookList {
+}
---
+++ src/madang/MainApp.java
@@ -0,0 +1,43 @@
+package madang;
+
+import java.sql.Connection;
+import java.util.Scanner;
+
+public class MainApp {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int menu = 0;
+    if(conn != null) {
+      while (true) {
+        menu = in.nextInt();
+        if(menu < 0) {
+          System.out.println("프로그램을 종료합니다.");
+          break;
+        }
+        switch (menu){
+          case 1:
+            bookList();
+            break;
+          case 2:
+            customerList();
+            break;
+          case 3:
+            orderList();
+            break;
+        }
+      }
+    }
+  }
+
+  static void bookList(){
+
+  }
+
+  static void customerList(){
+
+  }
+
+  static void orderList(){
+
+  }
+}
