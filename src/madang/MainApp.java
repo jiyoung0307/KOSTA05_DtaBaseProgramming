65aa65e046c00403373b2dabce5ce3e471147859
zy_zero <jiyoungjang0307@gmail.com>
2023-09-11 10:11:22 +0900

    git test

--- src/madang/MainApp.java
+++ src/madang/MainApp.java
@@ -4,13 +4,14 @@ import java.sql.Connection;
 import java.util.Scanner;
 
 public class MainApp {
+  static Connection conn = DBConn.makeConnection();
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
-    int menu = 0;
+    int menu =0;
     if(conn != null) {
-      while (true) {
+      while(true){
         menu = in.nextInt();
-        if(menu < 0) {
+        if(menu < 0){
           System.out.println("프로그램을 종료합니다.");
           break;
         }
